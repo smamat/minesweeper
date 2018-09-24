2dbc07d5b30f1d6f0d95d3ffec959f2ca3a3de12
smamat <smamat@gmail.com>
2018-09-24 17:42:42 +0800

    Build basic board with basic eventhandling

--- src/App.js
+++ src/App.js
@@ -3,22 +3,67 @@ import logo from './logo.svg';
 import './App.css';
 
 class Square extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClicked: false,
+    };
+  }
+
+  handleClick() {
+    console.log("handler in Square");
+    this.setState({isClicked: true});
+    this.props.onClick();
+  }
+
   render () {
-    return (<button>{this.props.col}</button>);
+
+
+    return (
+      <button
+      className={this.state.isClicked ? "open" : "closed"}
+      onClick={() => this.handleClick()}>
+      {this.props.col}
+      </button>
+    );
   }
 }
 
-class Board extends Component {
+function CurrentSquare(props) {
+  if (props.coord) {
+      return <b>({props.coord[0]},{props.coord[1]})</b>;
+  }
+  return <b>(X,X)</b>;
 
-  renderCol(dim) {
+}
+
+class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currSquare: undefined,
+    };
+  }
+
+  handleClick(c,r) {
+    console.log("square("+c+","+r+")");
+    this.setState({currSquare: [c,r]});
+
+  }
+
+  renderCol(r,dim) {
     // poor: making a list of integer so .map() can traverse
     var colIdx = [];
     for (var i=0; i < dim; i++){
       colIdx.push(i);
     }
 
-    const col = colIdx.map((sq,i) => {
-      return <Square key={sq} col={i}/>;
+    const col = colIdx.map((sq,c) => {
+      return (
+        <Square key={sq}
+        col={c} row={r}
+        onClick={()=>{this.handleClick(c,r)}}
+        />);
     });
 
     return col;
@@ -31,17 +76,21 @@ class Board extends Component {
     }
 
     const row = rowIdx.map((row,i) => {
-      return <div key={row}>{this.renderCol(dim)}</div>;
+      return <div key={row}>{this.renderCol(i,dim)}</div>;
     });
 
     return row;
   }
 
   render() {
+    const currSq = this.state.currSquare;
+
     return (
       <div>
       Board here
-      {this.renderRow(5)}
+      <br/>
+      <CurrentSquare coord={currSq} />
+      {this.renderRow(10)}
       </div>
     );
 
